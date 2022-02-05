f7efc549da1930390622de2c2461d6ccafc733ce
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2022-02-05 14:45:50 +0530

    module 5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,21 @@ import {
   addnewProduct,
   getProducts,
   getProductWithID,
+  deleteProduct,
+  updateProduct,
 } from "../controllers/controllers";
 
 const routes = (app) => {
   // Post endpoint - get multiple products
-  app.route('/products').get(getProducts).post(addnewProduct);
+  app.route("/products").get(getProducts).post(addnewProduct);
 
-  //   get a specific product with an id
-  app.route('/products/:ProductID').get(getProductWithID);
+  //   get a specific product with an id  also  update single item
+  //   delete product
+  app
+    .route("/products/:ProductID")
+    .get(getProductWithID)
+    .put(updateProduct)
+    .delete(deleteProduct);
 };
 
 export default routes;

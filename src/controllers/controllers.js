737b71ec9d4dc45079ce8e5d91ca748bde7e1439
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2022-02-05 15:01:09 +0530

    module 5

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -33,11 +33,8 @@ export const getProductWithID = (req, res) => {
 };
 
 export const updateProduct = (req, res) => {
-  Product.findOneAndUpdate(
-    { _id: req.params.ProductID },
-    req.body,
-    { new: true, useFindAndModify: false },
-    (err, Product) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product)
+     => {
       if (err) {
         res.send(err);
       }
@@ -47,10 +44,10 @@ export const updateProduct = (req, res) => {
 };
 
 export const deleteProduct = (req, res) => {
-  Product.deleteOne({ _id: req.params.ProductID }, (err, Product) => {
+    Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {
     if (err) {
       res.send(err);
     }
-    res.json({ message: "successfully deleted product" });
+    res.json({ message: 'successfully deleted product' });
   });
 };

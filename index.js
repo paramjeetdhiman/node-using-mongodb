54839577f9713c4e3f677ee51e914b8925d6e6e8
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2022-02-05 12:40:18 +0530

    mongoose connection

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/productsdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
